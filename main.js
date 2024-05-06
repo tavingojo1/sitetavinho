06345047bc4d21e948f912715bd96402e26d8119
tavingojo1 <108075672+tavingojo1@users.noreply.github.com>
2024-05-06 10:15:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = Document.querySelectorALL(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+    for[let j=0;j<botoes.length;]
+
+        botoes[i].classlist.add("ativo");
+    }
+    console.log(botoes [i]);
+}
